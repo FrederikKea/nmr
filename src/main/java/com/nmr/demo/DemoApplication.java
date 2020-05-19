57ff94f334b853ad8a85df7e55b0b138578f3187
Son Harry <sonharry.nguyen@gmail.com>
2020-05-19 22:31:02 +0200

    Udfyldt application.properties og testkode for forbindelse til db.

--- src/main/java/com/nmr/demo/DemoApplication.java
+++ src/main/java/com/nmr/demo/DemoApplication.java
@@ -1,13 +1,40 @@
 package com.nmr.demo;
 
+import com.nmr.demo.Util.DatabaseConnectionManager;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.sql.Statement;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 
 @SpringBootApplication
 public class DemoApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(DemoApplication.class, args);
-    }
 
+
+    }
 }
+
+//TEST KODE FOR FORBINDELSE TIL DB
+
+//        Connection mycon = com.nmr.demo.Util.DatabaseConnectionManager.getDatabaseConnection();
+//
+//        Statement myStatement;
+//        //test of database, prints out all customer names
+//        try {
+//            myStatement = mycon.createStatement();
+//            String getAllCustomers = "SELECT * FROM nmr.customer_table";
+//            ResultSet allCustomers = myStatement.executeQuery(getAllCustomers);
+//            while (allCustomers.next()) {
+//                System.out.println(
+//                        "Kunde-id : " + allCustomers.getString("customer_first_name")
+//                );
+//            }
+//        } catch (SQLException e) {
+//            e.printStackTrace();
+//        }
+
