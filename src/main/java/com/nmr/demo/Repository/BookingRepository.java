8f1d928a3b888bb2e7ec443e4c6e9cdded175e5f
Jakob Madsen <Fuzziwig@programmer.net>
2020-05-25 21:55:34 +0200

    Booking Interface og Repository tilføjet Mindre ændring til redigerkunde.html validering af dob

--- src/main/java/com/nmr/demo/Repository/BookingRepository.java
+++ src/main/java/com/nmr/demo/Repository/BookingRepository.java
@@ -1,4 +1,42 @@
 package com.nmr.demo.Repository;
 
-public class BookingRepository {
+import com.nmr.demo.Model.Booking;
+import com.nmr.demo.Util.DatabaseConnectionManager;
+
+import java.sql.*;
+import java.util.List;
+
+public class BookingRepository implements IBookingRepository {
+
+    private Connection conn;
+    private PreparedStatement preparedStatement;
+
+    public BookingRepository(){
+        this.conn = DatabaseConnectionManager.getDatabaseConnection();
+    }
+
+    @Override
+    public void createBooking(Booking b) {
+
+    }
+
+    @Override
+    public List<Booking> readAllBookings() {
+        return null;
+    }
+
+    @Override
+    public Booking readOneBooking(int id) {
+        return null;
+    }
+
+    @Override
+    public void updateBooking(Booking b) {
+
+    }
+
+    @Override
+    public void deleteBooking(int id) {
+
+    }
 }
--- src/main/java/com/nmr/demo/Repository/IBookingRepository.java
+++ src/main/java/com/nmr/demo/Repository/IBookingRepository.java
@@ -1,4 +1,19 @@
 package com.nmr.demo.Repository;
 
+import com.nmr.demo.Model.Booking;
+
+import java.util.List;
+
 public interface IBookingRepository {
+
+    // CRUD operations
+    void createBooking(Booking b);
+
+    List<Booking> readAllBookings();
+
+    Booking readOneBooking(int id);
+
+    void updateBooking(Booking b);
+
+    void deleteBooking(int id);
 }
